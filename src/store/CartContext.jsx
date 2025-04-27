195a809aa99498c496b0ea13dcac22d6a3224c25
Mohamed Ali <mohamed.ali14@vodafone.com>
2025-04-27 20:54:46 +0300

    Updated project files

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -14,40 +14,61 @@ const CartContext = createContext({
 
 function cartReducer(state, action) {
   if (action.type === "ADD") {
+    let updatedState = [...state];
     // Check if the item is already in the cart
     const itemExists = state.find((cartItem) => cartItem.id === action.id);
     // If the item is already in the cart, update its quantity
     if (itemExists) {
-      return state.map((cartItem) =>
+      updatedState = updatedState.map((cartItem) =>
         cartItem.id === action.id
           ? { ...cartItem, quantity: cartItem.quantity + 1 }
           : cartItem
       );
+    } else {
+      // If the item is not in the cart, add it to the cart
+      updatedState = [...updatedState, { id: action.id, quantity: 1 }];
     }
-    // If the item is not in the cart, add it to the cart
-    return [...state, { id: action.id, quantity: 1 }];
+
+    // update the local storage with the new cart state
+    localStorage.setItem("cart", JSON.stringify(updatedState));
+    // return the updated state
+    return updatedState;
   }
 
   if (action.type === "REMOVE") {
-    return [...state.filter((item) => item.id !== action.id)];
+    let updatedState = [...state];
+    updatedState = updatedState.filter((item) => item.id !== action.id);
+    // update the local storage with the new cart state
+    localStorage.setItem("cart", JSON.stringify(updatedState));
+    return updatedState;
   }
 
   if (action.type === "REMOVE_ONE") {
+    let updatedState = [...state];
     // Check if the item is already in the cart
-    const itemExists = state.find((cartItem) => cartItem.id === action.id);
+    const itemExists = updatedState.find(
+      (cartItem) => cartItem.id === action.id
+    );
     // If the item is already in the cart and more than 1, update its quantity
     if (itemExists.quantity > 1) {
-      return state.map((cartItem) =>
+      updatedState = updatedState.map((cartItem) =>
         cartItem.id === action.id
           ? { ...cartItem, quantity: cartItem.quantity - 1 }
           : cartItem
       );
+    } else {
+      // If the item is not in the cart, add it to the cart
+      updatedState = updatedState.filter((item) => item.id !== action.id);
     }
+    // update the local storage with the new cart state
+    localStorage.setItem("cart", JSON.stringify(updatedState));
     // If the item quantity in the cart is only one, remove it to the cart
-    return [...state.filter((item) => item.id !== action.id)];
+    return updatedState;
   }
 
   if (action.type === "CLEAR") {
+    // Clear the cart and remove it from local storage
+    localStorage.removeItem("cart");
     return [];
   }
 
@@ -55,7 +76,11 @@ function cartReducer(state, action) {
 }
 
 export default function CartProvider({ children }) {
-  const [cart, dispatch] = useReducer(cartReducer, []);
+  // Initialize the cart state from local storage or set it to an empty array
+  const storedCart = localStorage.getItem("cart");
+  const initialCart = storedCart ? JSON.parse(storedCart) : [];
+  // Initialize the cart state using useReducer
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
   const [filteredMeals, setFilteredMeals] = useState(availableMeals);
 
   function addToCart(id) {
